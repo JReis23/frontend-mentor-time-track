c303858c32d9357411909dc45cc30753489742d8
JReis23 <cjfreis23@gmail.com>
2022-02-12 17:37:27 +0100

    vercel ready

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,31 @@
-import adapter from '@sveltejs/adapter-node';
+// import adapter from '@sveltejs/adapter-node';
+// import preprocess from 'svelte-preprocess';
+
+// export default {
+// 	kit: {
+// 		adapter: adapter({
+// 			out: 'build',
+// 			precompress: false,
+// 			env: {
+// 				host: 'MYHOST',
+// 				port: 'MYPORT'
+// 			}
+// 		})
+// 	}
+// };
+
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
 	kit: {
-		adapter: adapter({
-			out: 'build',
-			precompress: false,
-			env: {
-				host: 'MYHOST',
-				port: 'MYPORT'
-			}
-		})
+		adapter: adapter()
 	}
 };
+
+export default config;
